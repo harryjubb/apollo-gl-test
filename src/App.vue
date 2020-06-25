5610d896342a63d123cb31e32109cf39f30219eb
Harry Jubb <harry@cresset-group.com>
2020-06-25 12:15:54 +0100

    Reproduced clash between vue-apollo and vue-golden-layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <golden-layout
+      class="goldenlayout"
+    >
+      <gl-row>
+        <gl-component title="component1">
+          <h1>Component 1</h1>
+        </gl-component>
+        <gl-stack>
+          <gl-component title="component2">
+            <h1>Component 2</h1>
+          </gl-component>
+          <gl-component title="component3">
+            <h1>Component 3</h1>
+          </gl-component>
+        </gl-stack>
+      </gl-row>
+    </golden-layout>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,40 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import vgl from 'vue-golden-layout'
+import 'golden-layout/src/css/goldenlayout-light-theme.css'
+
+import VueApollo from 'vue-apollo'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+
+// Apollo setup
+const httpLink = createHttpLink({
+  uri: 'https://graphql.org/swapi-graphql/'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+Vue.use(vgl)
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
